dde2fd75124902abe26bcc4e4476508a4f408e62
Daniel Hanggi <daniel.hanggi@braze.com>
2022-09-24 16:09:08 -0400

    Update .homebrew.sh

--- homebrew/.homebrew.sh
+++ homebrew/.homebrew.sh
@@ -5,9 +5,9 @@ brew upgrade
 
 BREW_PREFIX=$(brew --prefix)
 
-brew install android-platform-tools
+brew install base64
+brew install bash-completion
 brew install bitwarden-cli
-# brew install brew-cask-completion
 brew install coreutils
 brew install docker-compose
 brew install exiftool
@@ -17,66 +17,68 @@ brew install gawk
 brew install gh
 brew install git
 brew install git-absorb
-brew install git-completion
-brew install gmp
-brew install gnu-indent
 brew install gnu-sed
-brew install gnu-set
 brew install gnu-tar
-brew install gnupg
 brew install grep
-brew install imagemagick --with-webp
-# brew install jq
-# brew install mercurial
-# brew install moreutils
-# brew install ocaml
-# brew install openssh
-# brew install python3
+brew install homeassistant-cli
+brew install imagemagick
+brew install jq
+brew install mercurial
+brew install moreutils
+brew install openssh
+brew install prettier
+brew install python3
 brew install rclone
+brew install readline
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
 brew install tmux
 brew install tree
-brew install vim --with-override-system-vi
-brew install wget --with-iri
+brew install vim
+brew install wget
+brew install yamllint
 brew install yarn
-brew install xcode
+brew install yq
 brew install zsh-completion
 
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-brew install --cask aldente
 brew install --cask android-messages
-brew install --cask atom
+brew install --cask android-platform-tools
 # brew install --cask bitwarden # App Store version allows Touch ID
 brew install --cask discord
 brew install --cask docker
+brew install --cask element
+brew install --cask finicky
+brew install --cask firefox
 brew install --cask github
+brew install --cask gimp
 brew install --cask google-backup-and-sync
 brew install --cask google-chrome
 brew install --cask handbrake
-brew install --cask java
-# brew install --cask kdiff3
-# brew install --cask logitech-options
+brew install --cask home-assistant
+brew install --cask hyperbackupexplorer
+# brew install --cask intellij
+brew install --cask maccy
 brew install --cask makemkv
-# brew install --cask meld
+brew install --cask messenger
 brew install --cask musescore
-# brew install --cask origami-studio
 brew install --cask plex
 brew install --cask pocket-casts
-brew install --cask pokerstars
+# brew install --cask pokerstars
 brew install --cask postman
 brew install --cask rawtherapee
-# brew install --cask rectangle # Magnet alternative
-# brew install --cask slack
+brew install --cask rectangle
+brew install --cask rocket
+# brew install --cask rubymine
+brew install --cask slack
 brew install --cask spotify
 brew install --cask steam
-# brew install --cask storybook
-# brew install --cask virtualbox
+brew install --cask virtualbox
 brew install --cask visual-studio-code
 brew install --cask vlc
-# brew install --cask wireshark
-brew install --cask zoom
+# brew install --cask zoom
+
+brew tap homebrew/cask-drivers
+brew install --cask logitech-options
 
 brew cleanup
