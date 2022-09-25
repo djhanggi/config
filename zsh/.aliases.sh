0731e9beaa089937bcbc963d4dda288a228b3185
Daniel Hanggi <daniel.hanggi@braze.com>
2022-09-24 21:51:42 -0400

    Clean up .aliases.sh

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -1,54 +1,31 @@
-alias ll='ls -l'
+alias aliases='print -rl -- ${(k)aliases} | sort'
+alias reload='source ~/.config/zsh/.zshrc'
+
 alias ls='ls -GFh'
+alias ll='ls -l'
 
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='grm -vI'
 
+alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
-alias grep='grep --color=auto'
-
-alias reload='source ~/.config/zsh/.zshrc'
 
 alias history='history 0'
+alias h='history'
 alias history-grep='history | grep '
-alias histg=history-grep
+alias hig='history-grep'
 
 alias map='xargs -n1'
-
-alias sort-cb='pbpaste | sort | uniq | pbcopy'
-
-find-hidden() {
-	find ${1:-.} -name '.?*';
-}
-
-rm-hidden() {
-	find ${1:-.} -name .DS_Store -exec rm -v {} \;
-	find ${1:-.} -type d -name .Spotlight_V100 -exec rm -rv {} \;
-	find ${1:-.} -type d -name .fseventsd -exec rm -rv {} \;
-}
-
-find-capitalized-extensions() {
-	find -E ${1:-.} -type f -regex '.*\.[A-Z]+$'
-}
-
-lowercase-extension() {
-	rename -fv 's/\.([^.]+)$/.\L$1/' $1
-}
-
-lowercase-capitalized-extensions() {
-	REGEX='.*\.(PDF|JPG|PNG|GIF|TIF|CSV|DOC|DOCX|XLS|XLSX|PPT|PPTX|BMP|MTS)$'
-	find -E ${1:-.} -regex $REGEX -exec rename -fv 's/\.([^.]+)$/.\L$1/' {} \;
-}
 
 alias localip='ipconfig getifaddr en0'
 alias remoteip='curl http://ipecho.net/plain; echo'
 
+alias sort-cb='pbpaste | sort | uniq | pbcopy'
+
+alias bespin='ssh djhanggi@bespin.local'
 alias desktop='cd $HOME/Desktop'
 alias downloads='cd $HOME/Downloads'
-alias drive='cd "$HOME/Google Drive"'
 
-alias vscode='code'
 
-alias rsync-archive='rsync -ahHv --no-p --itemize-changes --progress --dry-run'
