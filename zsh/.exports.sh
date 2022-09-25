132743ea1c6218f2cd6234738c5220475568313b
Daniel Hanggi <daniel.hanggi@braze.com>
2022-09-24 23:22:16 -0400

    Add $PROMPT

--- zsh/.exports.sh
+++ zsh/.exports.sh
@@ -3,7 +3,8 @@ export EDITOR='vim'
 export EXIFTOOL_HOME=~/.config/exiftool/
 export HOMEBREW_CASK_OPTS='--appdir=/Applications'
 export INPUTRC=~/.config/readline/.inputrc
-export PS0='%n@%m:%~ %T $ '
+export PROMPT='%n@%m:%~ %T $ '
+export PS0=$PROMPT
 export VIMINIT='source ~/.vim/.vimrc'
 export XDG_CONFIG_HOME=~/.config/
 export ZDOTDIR=~/.config/zsh/
