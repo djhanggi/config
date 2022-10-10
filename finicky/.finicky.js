794bc7e5817585f1cb931cac3939af5085d5fa9e
Daniel Hanggi <daniel.hanggi@braze.com>
2022-10-09 22:20:32 -0400

    Update finicky (open Bitwarden links  or any option-clicked link in Firefox; redirect to smile.amazon.com)

--- finicky/.finicky.js
+++ finicky/.finicky.js
@@ -1,12 +1,27 @@
 module.exports = {
   defaultBrowser: "Google Chrome",
+  options: {
+    hideIcon: true,
+    logRequests: false,
+  },
   handlers: [
     {
       match: [
-        /^https?:\/\/tamacteam\.slack\.com\/.*$/,
+        () => finicky.getKeys().option,
         ({ url }) => url.host.startsWith(192.168),
+        ({ opener }) => opener.path?.includes("Bitwarden"),
+        /^https?:\/\/tamacteam\.slack\.com\/.*$/,
       ],
       browser: "Firefox",
+    },
+  ],
+  rewrite: [
+    {
+      match: finicky.matchDomains(["www.amazon.com"]),
+      url: ({ url }) => ({
+        ...url,
+        host: "smile.amazon.com",
+      }),
     },
   ],
 };
